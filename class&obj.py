e79b49a7ec33566c3e0c7124fa3987ed32490a49
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 13:22:32 +0530

    Create class&obj.py

---
+++ class&obj.py
@@ -0,0 +1,32 @@
+class Car:
+  b = Honda
+a = Car()
+print(a.b)
+
+#output--> Honda
+
+--------------------------------------
+
+class Vehicle:
+  def __init__(self, car, model):
+    self.car = car
+    self.model = model
+  def myfunc(self):
+    print("Hello this vehicle is " + self.car)
+v1 = Vehicle("Honda", X8)
+p1.myfunc()
+
+# Hello this vehicle is Honda 
+-------------------------------------------
+
+class Vehicle:
+  def __init__(mysillyobject, cat, model:
+    myobj.car = car
+    myobj.model = model
+  def myfunc(xyz):
+      print("Hello this vehicle is " + xyz.car)
+v1 = Vehicle("Honda", X8)
+p1.myfunc()
+
+# Hello this vehicle is Honda 
+-------------------------------------------
