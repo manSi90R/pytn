e218b536bf0f007a3b12c4bef835bb3360dec095
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:42:00 +0530

    Create q9.py

---
+++ q9.py
@@ -0,0 +1,10 @@
+# Write a Python program to calculate the sum of three given numbers, if the values are not - equal then return four times of their sum
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a==b==c:
+    print(a+b+c)
+else:
+    print(a*b*c)
