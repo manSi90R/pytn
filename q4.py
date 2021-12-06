d55a37a35eb77ce5db76731c9d13358be5d78215
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:38:15 +0530

    Create q4.py

---
+++ q4.py
@@ -0,0 +1,18 @@
+# Accept two numbers from the user and multiply them together.
+
+def multiply(num1,num2)
+    product=num1 * num2
+    return product
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+result=multiply(num1,num2)
+print("Multipication of two numbers: ",result)
+
+
+# output
+
+Enter first number: 2
+Enter second number: 20
+Multipication of two numbers: 40
