1f1003a23b9412420ecf9e7defe0296005d4ad68
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:41:08 +0530

    Create q7.py

---
+++ q7.py
@@ -0,0 +1,20 @@
+# Parse string into integer
+
+str = "1500"
+str = int(str)
+
+print(str)
+
+# Outout-
+# 1500
+
+
+# Parse string into float
+
+s = "1500.4"
+s = float(s)
+
+print(s)
+
+# Output-
+# 1500.4
