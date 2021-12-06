7cf878e7eb5f76b6781df3448a5ba3c54e0949d6
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:40:02 +0530

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,10 @@
+# Write a Python program to calculate the length of a string
+
+str = input("Enter a string: ")
+
+print("Length of the input string is:", len(str))
+
+
+# Output:
+# Enter a string: Acropolis
+# Length of the input string is: 9
