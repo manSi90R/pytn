a5c72e6c20047921f3524230552e31e23d1ecf82
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:46:35 +0530

    Create q16.py

---
+++ q16.py
@@ -0,0 +1,7 @@
+x = int(input("enter number of classess held:"))
+y = int(input("enter number of classess attended:"))
+z = int(input("percentage of classess attended:"))
+if z>80:
+    print("can sit in exam")
+else:
+    print("can not sit in exam")
