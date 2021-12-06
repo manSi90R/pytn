4d09d91b232ef94d29fb95af7d90cd4b6f1b4a5e
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:45:23 +0530

    Create q14.py

---
+++ q14.py
@@ -0,0 +1,4 @@
+a1, a2, a3 = [int(x) for x in input("Enter age of three persons: ").split()]
+
+print("The Oldest person is of age:", max(a1, a2, a3))
+print("The Youngest person is of age:", min(a1, a2, a3))
