51f634cf0e72ccdb31654db5a686cd81a41ab42c
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:37:07 +0530

    Create q2.py

---
+++ q2.py
@@ -0,0 +1,12 @@
+#Takes two integer numbers and  return their product.
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+pro=num1*num2;
+print("the product of given numbers is ",pro)
+
+output-
+
+Enter the first number: 23
+Enter the second number : 32
+the product of the given numbers is 736
