f6d3dc44826b47c0134f3bd82cc9b7fc0bc60503
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 13:28:11 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,23 @@
+# program to demonstrate String in Python
+s = 'titanic'
+print('s = ', s)
+
+#first character
+print('s[0] = ', s[0])
+
+#last character
+print('s[-1] = ', s[-1])
+
+#slicing 2nd to 5th character
+print('s[1:5] = ', s[1:5])
+
+#slicing 6th to 2nd last character
+print('s[5:-2] = ', s[5:-2])
+
+--------------------------------------
+Output
+s =  titanic
+s[0] =  T
+s[-1] =  c
+s[1:5] =  itani
+s[4:-2] =  ni
