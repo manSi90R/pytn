e3c3764f0e9a38bf0c7b805f4086890e31d011ea
manSi90R <91050977+manSi90R@users.noreply.github.com>
2021-12-06 12:43:27 +0530

    Create q11.py

---
+++ q11.py
@@ -0,0 +1,7 @@
+# To check whether a number is divisible by 8 and 12 or not.
+
+num = int(input())
+if num % 8 == 0 and num % 12 == 0:
+    print ("number is divisible by 8 and 12")
+else:
+    print("number is not divisible by 8 and 12")
